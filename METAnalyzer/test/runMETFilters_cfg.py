889fae24f9b8ab40c0e1fe8ed2639e1228a56a95
Seema <seema.sharma@cern.ch>
2017-06-07 17:34:04 -0500

    example to apply MET filters

---
+++ METAnalyzer/test/runMETFilters_cfg.py
@@ -0,0 +1,31 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+        #'root://cmseos.fnal.gov//store/user/vhegde/MET_HATS2017/WJetsToLNu_HT-400To600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_PUMoriond17_80X_1C34F84E-A8BE-E611-BE97-441EA1733CCC.root'#WJetsToLNu MC
+       'root://cmseos.fnal.gov//store/user/vhegde/MET_HATS2017/Run2016G_MET_MINIAOD_03Feb2017-v1_2E1DF27E-37EC-E611-A696-00259073E4E2.root'#Run 2016G MET dataset
+    )
+)
+
+process.demo = cms.EDAnalyzer('METFilters',
+                              metSrc = cms.untracked.InputTag("slimmedMETs"),
+                              noiseFilterTag = cms.InputTag("TriggerResults"),
+
+                              GoodVtxNoiseFilter_Selector_ = cms.string("Flag_goodVertices"),
+                              GlobalTightHalo2016NoiseFilter_Selector_ = cms.string("Flag_globalTightHalo2016Filter"),
+                              HBHENoiseFilter_Selector_ = cms.string("Flag_HBHENoiseFilter"),
+                              HBHENoiseIsoFilter_Selector_ = cms.string("Flag_HBHENoiseIsoFilter"),
+                              EcalDeadCellTriggerPrimitiveNoiseFilter_Selector_ = cms.string("Flag_EcalDeadCellTriggerPrimitiveFilter"),
+                              EEBadScNoiseFilter_Selector_ = cms.string("Flag_eeBadScFilter")
+)
+
+process.p = cms.Path(process.demo)
