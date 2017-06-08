8ba0c22c3fcb9e7ce232a0304d6578375d013ca2
Seema <seema.sharma@cern.ch>
2017-06-08 17:17:53 -0500

    example to access MET uncertainties

--- METAnalyzer/test/runMETUncertainties_cfg.py
+++ METAnalyzer/test/runMETUncertainties_cfg.py
@@ -10,7 +10,6 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
-        #'root://cmseos.fnal.gov//store/user/vhegde/MET_HATS2017/Run2016G_MET_MINIAOD_03Feb2017-v1_2E1DF27E-37EC-E611-A696-00259073E4E2.root'#Run 2016G MET dataset
         # MC file
         'root://cms-xrd-global.cern.ch//store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/120000/006F1B9A-81D0-E611-B9CE-0025905AA9CC.root'
         )
